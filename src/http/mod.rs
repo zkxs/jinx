9a426674fa2cfae25d04f8191a60f4bf2d17a76f
Michael Ripley <zkxs00@gmail.com>
2025-02-06 19:11:43 -0600

    Increase jinxxy timeout, decrease github timeout

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -15,8 +15,8 @@ static HTTP1_CLIENT: LazyLock<reqwest::Client> = LazyLock::new(|| {
         .user_agent(constants::USER_AGENT)
         .gzip(true)
         .https_only(true)
-        .connect_timeout(Duration::from_secs(10))
-        .timeout(Duration::from_secs(10))
+        .connect_timeout(Duration::from_secs(20))
+        .timeout(Duration::from_secs(20))
         // .connection_verbose(true) // useful for debugging
         .build()
         .expect("Failed to build HTTP1 client")
@@ -31,8 +31,8 @@ static HTTP2_CLIENT: LazyLock<reqwest::Client> = LazyLock::new(|| {
         // .http2_keep_alive_timeout(Duration::from_secs(10)) // if the ping is not acknowledged within the timeout, the connection will be closed
         .gzip(true)
         .https_only(true)
-        .connect_timeout(Duration::from_secs(10))
-        .timeout(Duration::from_secs(10))
+        .connect_timeout(Duration::from_secs(7))
+        .timeout(Duration::from_secs(7))
         // .connection_verbose(true) // useful for debugging
         .build()
         .expect("Failed to build HTTP2 client")
